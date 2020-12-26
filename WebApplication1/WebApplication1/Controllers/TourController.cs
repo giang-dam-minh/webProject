e443a0b29d8e720ffbfef7dcdfd982d93becda30
giang-dam-minh <minhgiang851@gmail.com>
2020-12-26 12:02:13 +0700

    second

---
+++ WebApplication1/WebApplication1/Controllers/TourController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TourController : Controller
+    {
+        // GET: Tour
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult tourTrongNuoc()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/js/slide.js
@@ -0,0 +1,53 @@
+﻿$(document).ready(function () {
+    //Your code
+});
+var stt = 0;
+var endImg = $("img.slide:last").attr("idx");
+var interval;
+var timer = function () {
+    interval = setInterval(function () {
+        $("#next").click();
+    }, 5000);
+};
+
+timer();
+$("img.slide").hide();
+$("img.slide").eq(stt).show();
+$("button").removeClass("active");
+$("button").eq(stt).addClass("active");
+var changeImg = function (stt) {
+    $("img.slide").hide();
+    $("img.slide").eq(stt).show();
+    $("button").removeClass("active");
+    $("button").eq(stt).addClass("active");
+
+    clearInterval(interval);
+    timer();
+};
+
+$("button").click(function () {
+    stt = $(this).attr("idx");
+
+    changeImg(stt);
+});
+
+$("#next").click(function () {
+    if (++stt > endImg) {
+        stt = 0;
+    }
+
+    changeImg(stt);
+});
+
+$("#prev").click(function () {
+    if (--stt < 0) {
+        stt = endImg;
+    }
+
+    changeImg(stt);
+});
+etInterval(function () {
+    $("#next").click();
+}, 5000);
+$("img.slide").eq(stt).show();
+$("img.slide").eq(stt).fadeIn(500);
