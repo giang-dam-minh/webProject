a1cc173db4d1fe7096afe5a0cfbf54c4a3e5d212
giang-dam-minh <minhgiang851@gmail.com>
2021-01-04 16:10:43 +0700

    4/1/2021 second

--- WebApplication1/WebApplication1/Controllers/TourController.cs
+++ WebApplication1/WebApplication1/Controllers/TourController.cs
@@ -25,5 +25,9 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public ActionResult tourDetail()
+        {
+            return View();
+        }
     }
 }
